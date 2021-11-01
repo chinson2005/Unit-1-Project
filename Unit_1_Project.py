d096bbffcee0ae6d5a873f9b57752042328f8a35
chinson2005 <93168543+chinson2005@users.noreply.github.com>
2021-11-01 17:46:04 -0500

    Create Unit_1_Project.py
    Number guessing game with high score, and option to replay after completing.

---
+++ Unit_1_Project.py
@@ -0,0 +1,46 @@
+#  Reject if exceeds not met
+
+import random
+high_score = [10]
+
+    
+def game():
+    attempts = 1
+    ans = random.randint(1, 10)
+    print(ans)  # !!!LEFT IN FOR EASIER TESTING!!! Will be removed before submission
+    print("Welcome to the Number Guessing Game!\nThe high score is {}.\n".format(min(high_score)))
+    while True:
+        try:
+            guess = int(input("Guess a number 1-10: "))
+            if guess < 1 or guess > 10:
+                attempts += 1
+                print("Sorry, your guess is outside of the range, please try again: ")
+                continue
+            elif guess < ans:
+                attempts += 1
+                print("It's higher: ")
+                continue
+            elif guess > ans:
+                attempts += 1
+                print("It's lower: ")
+                continue
+            else:
+                if attempts == 1:
+                    tries = "try"
+                else:
+                    tries = "tries"
+                print("\nThat is correct!!! You guessed it in {} {}!".format(attempts, tries))
+                high_score.append(attempts)
+                play_again = input("\nWould you like to play again?  Yes/No: ")
+                if play_again.lower() == 'yes':
+                    game()
+                else:
+                    print("\nThank you for playing!")
+            break
+
+        except ValueError:
+            attempts += 1
+            print("Please use integers (whole numbers)")
+
+
+game()
